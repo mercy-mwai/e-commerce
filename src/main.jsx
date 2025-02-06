44b56959a7343b8cbf494515abac70e6c848bb0d
mercy-mwai <mercyneemam@gmail.com>
2025-02-06 11:17:20 +0300

    created a new file Banner.jsx in the home folder used to dispaly banner content moved home folder to the components folder added path blog to my main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import './index.css'
 import App from './App.jsx'
-import Home from "./Home.jsx";
+import Home from "./home/Home.jsx";
 import Blog from "./Blog.jsx";
 import 'swiper/css';
 
