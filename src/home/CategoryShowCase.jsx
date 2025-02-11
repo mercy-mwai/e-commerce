c3b179dc835000e24d598defce1c693cdd084144
mercy-mwai <mercyneemam@gmail.com>
2025-02-11 09:22:50 +0300

    added shapes and unordered list

--- src/home/CategoryShowCase.jsx
+++ src/home/CategoryShowCase.jsx
@@ -79,23 +79,35 @@ const ProductData = [
 
 const CategoryShowCase = () => {
   const [items, setItems] = useState(ProductData);
+
+  const filterItem=()=>{
+
+  };
   return (
-    <div className="course-sections style-3 padding-tb">
-      {/*shapes*/}
-      <div className="course-shape one">
-        <img src="/src/assets/images/shape-img/icon/01.png" alt="" />
-      </div>
-      <div className="course-shape two">
-        <img src="/src/assets/images/shape-img/icon/02.png" alt="" />
-      </div>
-      {/*main container*/}
-      <div className="container">
-         {/*section header*/}
-         <div className="section-header">
-          <h2 className="title">{title}</h2>
-         </div>
+   <div className="course-section style-3 padding-tb">
+    {/*shapes*/}
+    <div className="course-shape one">
+      <img src="/src/assets/images/shape-img/icon/01.png"alt="" />
+    </div>
+    <div className="course-shape two">
+    <img src="/src/assets/images/shape-img/icon/02.png"alt="" />
+    </div>
+    {/*main section*/}
+    <div className="container">
+      <div className="section-header">
+        <h2 className="title">{title}</h2>
+        <div className="course-filter-group">
+          <ul className="lab-ul">
+            <li onClick={()=>filterItem("All")} >All</li>
+            <li onClick={()=>filterItem("Shoes")} >Shoes</li>
+            <li onClick={()=>filterItem("Bags")} >Bags</li>
+            <li onClick={()=>filterItem("Phones")} >Phones</li>
+            <li onClick={()=>filterItem("Beauty")} >Beauty</li>
+          </ul>
+        </div>
       </div>
     </div>
+   </div>
   );
 };
 
