1e25059377477e24180bc7f3c5fa0b5eab52cd2f
mercy-mwai <mercyneemam@gmail.com>
2025-02-18 12:52:35 +0300

    added an image on the left side and iconfont to navigate through imported swipper.jsx

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,47 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import { data, useParams } from 'react-router-dom';
-import PageHeader from '../components/PageHeader';
+import React, { useEffect, useState } from "react";
+import { data, useParams } from "react-router-dom";
+import PageHeader from "../components/PageHeader";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
+import { Autoplay } from "swiper/modules";
 
 const SingleProduct = () => {
-    const [product, setProduct]=useState([]);
-    const {id}=useParams();
-    //console.log(id)
-    useEffect( ()=>{
-        fetch("/src/products.json").then(response=> response.json()).then(productData=>setProduct(productData))
-    }, []);
+  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+  //console.log(id)
+  useEffect(() => {
+    fetch("/src/products.json")
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, []);
 
-const result =product.filter((p)=> p.id===id);
-console.log(result)
+  const result = product.filter((p) => p.id === id);
+  console.log(result);
 
   return (
     <div>
-        <PageHeader title={"OUR SHOP SINGLE"} curPage={"Shop/ SingleProduct"} />
-        <div className='shop-single padding-tb aside-bg'>
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className='col-lg-8 col-12'>
-                        <article>
-                            <div className="product-details">
-                                <div className='row align-items-center'>
-                                    <div className='col-md-6 col-12'>Image</div>
-                                    <div className='col-md-6 col-12'>Details</div>
+      <PageHeader title={"OUR SHOP SINGLE"} curPage={"Shop / SingleProduct"} />
+      <div className="shop-single padding-tb aside-bg">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                <div className="product-details">
+                  <div className="row align-items-center">
+                    <div className="col-md-6 col-12">
+                      <div className="product-thumb">
+                        <div className="swiper-container pro-single-top">
+                          <Swiper
+                            spaceBetween={30}
+                            slidesPerView={1}
+                            loop={true}
+                            autoplay={{
+                              delay: 2000,
+                              disableOnInteraction: false,
+                            }}
+                            modules={[Autoplay]}
+                            navigation={{
+                              prevEl: ".pro-single-prev",
+                              nextEl: ".pro-single-next",
+                            }}
+                            className="mySwiper"
+                          >
+                            {result.map((item, i) => (
+                              <SwiperSlide key={i}>
+                                <div className="single-thumb">
+                                  <img src={item.img} alt="" />
                                 </div>
-                            </div>
+                              </SwiperSlide>
+                            ))}
+                          </Swiper>
 
-                            {/*reviews*/}
-                            <div className="review">
-                                Reviews
-                            </div>
-                        </article>
+                          {/* ✅ Fixed Navigation Icons */}
+                          <div className="pro-single-next">
+                            <i className="icofont-rounded-left"></i>
+                          </div>
+                          <div className="pro-single-prev">
+                            <i className="icofont-rounded-right"></i>
+                          </div>
+                        </div>
+                      </div>
                     </div>
 
-                    {/*right side*/}
-                    <div className='col-lg-4 col-12'>Right Side</div>
+                    {/* Product Details Section */}
+                    <div className="col-md-6 col-12">Details</div>
+                  </div>
                 </div>
-            </div>
-        </div>
-    </div>
-  )
-}
 
-export default SingleProduct
+                {/* Reviews Section */}
+                <div className="review">Reviews</div>
+              </article>
+            </div>
+
+            {/* Right Sidebar */}
+            <div className="col-lg-4 col-12">Right Side</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  
+};
+
+export default SingleProduct;
