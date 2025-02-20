65ac49a7ed597b9d77590ea4d32f2572e5847a09
mercy-mwai <mercyneemam@gmail.com>
2025-02-20 12:33:23 +0300

    created CartPage UI by adding product image,price ,quantity and total price in a table format

--- src/shop/CartPage.jsx
+++ src/shop/CartPage.jsx
@@ -1,4 +1,6 @@
 import React, {useEffect,useState} from 'react'
+import PageHeader from '../components/PageHeader';
+import { Link } from 'react-router-dom';
 
 const CartPage=()=>{
     const [cartItems, setCartItems] =useState([]);
@@ -11,7 +13,7 @@ const CartPage=()=>{
     //calculate price
 
     const calculateTotalPrice= (item)=>{
-       return item.price  * item.quantity;
+       return item.price  += item.quantity;
     };
 
     //handle quantity increase
@@ -48,16 +50,68 @@ const CartPage=()=>{
     }
     
     //cart subtotal
-    const cartSubTotal =cartItems.reduce((total , item)=>{
+    /*const cartSubTotal =cartItems.reduce((total , item)=>{
         return total + calculateTotalPrice(item); 
-    });
+    }); */
+
+    const cartSubTotal = Array.isArray(cartItems)
+        ? cartItems.reduce((total, item) => total + calculateTotalPrice(item), 0)
+        : 0;
 
     //order total
     const orderTotal=cartSubTotal;
 
     
 return(
-    <div>Cart Page</div>
+    <div>
+        <PageHeader title={"Shop Cart"} curPage={"Cart Page"} />
+        <div className="shop-cart padding-tb">
+            <div className="container">
+                <div className="section-wrapper">
+                    {/* cart top */}
+                    <div className="cart-top">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th className='cat-product'>Product</th>
+                                    <th className='cat-price'>Price</th>
+                                    <th className='cat-quantity'>Quantity</th>
+                                    <th className='cat-toprice'>Total</th>
+                                    <th className='cat-edit'>Edit</th>
+                                </tr>
+                            </thead>
+                            {/* table body */}
+                            <tbody>
+                                {
+                                    cartItems.map((item, i)=>(
+                                        <tr key={i}>
+                                            <td className="product-item cat-product">
+                                                <div className="p-thumb">
+                                                    <Link to="/shop"><img src={item.img} alt='' /></Link>
+                                                </div>
+                                                <div className="p-content">
+                                                    <Link to="/shop">{item.name}</Link>
+                                                </div>
+                                            </td>
+                                            <td className='cat-price'> ${item.price}</td>
+                                            <td className="cat-quantity">
+                                                <div className="cart-plus-minus">
+                                                    <div className='dec qtybutton'>-</div>
+                                                    <input type='text' className='cart-plus-minus-box' name='qtybutton' value={item.quantity}  />
+                                                    <div className='inc qtybutton'>+</div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                    {/* cart top ending..... */}
+                </div>
+            </div>
+        </div>
+    </div>
 )
 }
 
