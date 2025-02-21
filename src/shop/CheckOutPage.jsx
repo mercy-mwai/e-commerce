8ee19be39436cf273321378c865b6d360aba8dba
mercy-mwai <mercyneemam@gmail.com>
2025-02-21 11:10:34 +0300

    created a Ul tag with li tag for the visa and paypall images

--- src/shop/CheckOutPage.jsx
+++ src/shop/CheckOutPage.jsx
@@ -1,42 +1,85 @@
-import { Modal } from 'react-bootstrap';
-import React , {useState} from 'react';
-import { Button } from 'react-bootstrap';
+import { Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
 const CheckOutPage = () => {
-    const [show, setShow] = useState(false);
-    const [activeTab, setActiveTab] = useState("Visa");
+  const [show, setShow] = useState(false);
+  const [activeTab, setActiveTab] = useState("Visa");
 
-//handle tab change
-const handleTabChange=(tabId)=>{
-    setActiveTab(tabId)
-}
+  //handle tab change
+  const handleTabChange = (tabId) => {
+    setActiveTab(tabId);
+  };
 
-const handleShow=()=> setShow(true);
-const handleClose=()=> setShow(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
   return (
-    <div className='modalCard'>
-        <Button variant='primary py-4' onClick={handleShow}>Proceed to Checkout</Button>
-        <Modal
-         show={show}
-         onHide={handleClose}
-         animation={false}
-         className="modal-fade"
-         centered
-        >
-            <div className="modal-dialog">
-                <h5 className='px-3 mb-3'> Select Your Payment Method</h5>
-                <div className="modal-content">
-                    <div className="modal-body">
-                        <div className='tabs mt-3'>
-
-                        </div>
-                    </div>
-                </div>
+    <div className="modalCard">
+      <Button variant="primary py-4" onClick={handleShow}>
+        Proceed to Checkout
+      </Button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        animation={false}
+        className="modal-fade"
+        centered
+      >
+        <div className="modal-dialog">
+          <h5 className="px-3 mb-3"> Select Your Payment Method</h5>
+          <div className="modal-content">
+            <div className="modal-body">
+              <div className="tabs mt-3">
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                  <li className="nav-item" role="presentation">
+                    <a
+                      id="visa-tab"
+                      data-toggle="tab"
+                      role="tab"
+                      aria-controls="visa"
+                      aria-selected={activeTab === "visa"}
+                      onClick={() => handleTabChange("visa")}
+                      href="#visa"
+                      className={`nav-link ${
+                        activeTab === "visa" ? "active" : ""
+                      }`}
+                    >
+                      <img
+                        src=" https://i.imgur.com/sB4jftM.png"
+                        alt=" "
+                        width="80"
+                      />
+                    </a>
+                  </li>
+                  <li className="nav-item" role="presentation">
+                    <a className={`nav-link ${
+                        activeTab === "paypal" ? "active" : ""
+                      }`}
+                      id="paypal-tab"
+                      data-toggle="tab"
+                      role="tab"
+                      aria-controls="paypal"
+                      aria-selected={activeTab === "Paypal"}
+                      onClick={() => handleTabChange("Paypal")}
+                      href="Paypal"
+                      
+                    >
+                      <img
+                        src="https://i.imgur.com/yK7EDD1.png"
+                        alt=" "
+                        width="80"
+                      />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-        </Modal>
+          </div>
+        </div>
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default CheckOutPage
+export default CheckOutPage;
