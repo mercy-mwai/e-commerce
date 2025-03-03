6521a112215a38de51ffccc7f98d37a2ed5ebe9c
mercy-mwai <mercyneemam@gmail.com>
2025-03-03 15:59:40 +0300

    Implemented user registration with email/password and Google authentication

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,155 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthProvider";
+
+const title = "Register";
+const socialTitle = "Login With Social Media";
+const btnText = "Sign up";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [errorMessage, setErrorMessage] = useState(" ");
+  const { signInWithGmail, createUser } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-export default Signup
+  const from = location.state?.from?.pathname || "/";
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+    const form =event.target;
+    //console.log(form);
+    const email =form.email.value;
+    const password =form.password.value;
+    const confirmPassword=form.confirmPassword.value;
+    //console.log(email ,password, confirmPassword)
+    if(password !== confirmPassword){
+        setErrorMessage("Password does not match! Please, provide correct password");
+    }else{
+        setErrorMessage("");
+        createUser(email ,password).then((userCredentials)=>{
+            const user =userCredentials.user;
+            alert("Account created Successfully");
+            navigate(from , {replace: true});
+        }).catch((error) =>{
+            console.log(error.message);
+            alert(`${error.message}`)
+        })
+    }
+  };
+
+  const handleRegister = (event) => {
+    signInWithGmail()
+      .then((result) => {
+        const user = result.user;
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        const errorMsg = error.message;
+        setErrorMessage("Please provide valid email & password");
+      });
+  };
+
+  return (
+    <div className="login-section padding-tb section-bg">
+      <div className="container">
+        <div className="account-wrapper">
+          <h3 className="title">{title}</h3>
+          <form className="account-form" onSubmit={handleSignup}>
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="UserName "
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Address "
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password "
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                required
+              />
+            </div>
+            {/*showing message */}
+            <div>
+              {errorMessage && (
+                <div className="error-message text-danger mb-1">
+                  {errorMessage}
+                </div>
+              )}
+            </div>
+
+            <div className="form-group">
+              <button type="submit" className="d-block lab-btn">
+                <span>{btnText}</span>
+              </button>
+            </div>
+          </form>
+
+          {/* account bottom */}
+          <div className="account-bottom">
+            <span className="d-block cate pt-10">
+              Have an Account? <Link to="/login">Login</Link>
+            </span>
+            <span className="or">
+              <span>or</span>
+            </span>
+
+            {/*social login */}
+            <h5 className="subtitle">{socialTitle}</h5>
+            <ul className="lab-ul social-icons justify-content-center">
+              <li>
+                <button className="github" onClick={handleRegister}>
+                  <i className="icofont-github"></i>
+                </button>
+              </li>
+              <li>
+                <a href="/" className="facebook">
+                  <i className="icofont-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="/" className="twitter">
+                  <i className="icofont-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="/" className="linkedin">
+                  <i className="icofont-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a href="/" className="instagram">
+                  <i className="icofont-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
