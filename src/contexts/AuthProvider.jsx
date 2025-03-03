68a3fec02f84c6e4a030596e9af7c7591d7a7d9d
mercy-mwai <mercyneemam@gmail.com>
2025-03-03 10:24:36 +0300

    Set up authentication context with user state management

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,10 +1,17 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react';
+import app from "../firebase/Firebase.config"
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
 const AuthContext =createContext();
+const auth=getAuth();
+const googleProvider= new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
+  const [user ,setUser]=useState(null);
+  const [loading, setLoading]= useState(true);
     const authInfo={
-       
+       user,
+       loading
     }
   return (
     <AuthContext.Provider value={authInfo}>
