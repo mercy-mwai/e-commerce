c19a6033ee6b6b9a4b72a29901e02ff56e2ce483
mercy-mwai <mercyneemam@gmail.com>
2025-03-03 13:13:08 +0300

    Added Login Component with Form and Social Authentication Support

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
+const title ="Login";
+const socialTitle ="Login With Social Media";
+const btnText ="Login Now"
+
+const socialList = [
+  { iconName: "icofont-facebook", siteLink: "#", className: "facebook" },
+  { iconName: "icofont-twitter", siteLink: "#", className: "twitter" },
+  { iconName: "icofont-linkedin", siteLink: "#", className: "linkedin" },
+  { iconName: "icofont-instagram", siteLink: "#", className: "instagram" },
+  { iconName: "icofont-pinterest", siteLink: "#", className: "pinterest" },
+];
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+const [errorMessage, setErrorMessage] = useState(" ");
+const {signInWithGmail, login} =useContext(AuthContext);
+const location =useLocation();
+const navigate= useNavigate();
 
-export default Login
+const from= location.state?.from?.pathname || "/";
+
+const handleLogin=(e)=>{
+
+}
+  return(
+    <div>
+        <div className="login-section padding-tb section-bg">
+            <div className="container">
+                <div className="account-wrapper">
+                    <h3 className="title">{title}</h3>
+                    <form className="account-form" onSubmit={handleLogin}>
+                        <div className="form-group">
+                            <input type="email" name="email" id="email" placeholder="Email Address *" required />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" name="password" id="password" placeholder="Password *" required />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default Login;
